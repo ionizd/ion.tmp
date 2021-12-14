3b33f7d4da1c76bda902f63f094cdd6b6c28dce2
Maciek Misztal <mmisztal1980@gmail.com>
2021-12-14 06:44:24 +0100

    build fixed

--- ion.microservices/ion-microsercvices-job-pkg/demo/Ion.MicroServices.Job.Demo/Services/JobService1.cs
+++ ion.microservices/ion-microsercvices-job-pkg/demo/Ion.MicroServices.Job.Demo/Services/JobService1.cs
@@ -5,26 +5,21 @@ namespace Ion.MicroServices.Job.Demo.Services;
 public class JobService1 : IHostedJobService
 {
     private readonly ILogger<JobService1> logger;
-
     public JobService1(ILogger<JobService1> logger)
     {
         this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
-
-    public bool Completed { get; }
-    public async Task StartAsync(CancellationToken cancellationToken)
+    public Task StartAsync(CancellationToken cancellationToken)
     {
         var rand = new Random(DateTime.UtcNow.Millisecond);
 
-        await TaskEx.TryWaitUntil(() => true == false,
+        return TaskEx.TryWaitUntil(() => true == false,
             500.Milliseconds(),
             rand.Next(3000, 10000).Milliseconds(),
             () =>
             {
                 logger.LogInformation($"{nameof(JobService1)} doing work ...");
             });
-
-        Completed = true;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
